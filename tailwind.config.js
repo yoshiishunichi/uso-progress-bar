803398979fcb3ca4ab755d5972d537332123a0eb
吉井駿一 <syun_syun01@icloud.com>
2022-09-06 01:10:24 +0900

    :wrench: tailwind.config.jsにtransitionPropertyの設定値を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   plugins: [],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        bg: "background",
+      },
+    },
   },
 };
