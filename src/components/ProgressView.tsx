5dca04d48142f4b04cccbdc94d6701837fea873e
吉井駿一 <syun_syun01@icloud.com>
2022-09-07 00:05:14 +0900

    :sparkles: 閉じるボタンを追加

--- src/components/ProgressView.tsx
+++ src/components/ProgressView.tsx
@@ -1,4 +1,5 @@
 import { FC, useMemo } from "react";
+import { IoClose } from "react-icons/io5";
 
 import { FetchState } from "pages";
 
@@ -12,9 +13,13 @@ const ProgressView: FC<ProgressViewProps> = ({ fetchingState }) => {
   return fetching || error ? (
     <div className="absolute inset-0 flex items-center justify-center bg-frosted">
       <div className="h-28 w-96 rounded-sm border border-black bg-slate-100">
-        <div className="relative flex h-6 items-center justify-center border-b border-black bg-gradient-to-b from-header-thin to-header-dark">
+        <div className="relative flex h-8 items-center justify-center border-b border-black bg-gradient-to-b from-header-thin to-header-dark">
           <p className="text-center text-xs">Progress Bar</p>
-          <button className="absolute left-1 h-3 w-3 rounded-full bg-close"></button>
+          <button className="absolute left-2 h-4 w-4 rounded-full bg-close">
+            <div className="flex h-4 w-4 items-center justify-center">
+              <IoClose size={12} />
+            </div>
+          </button>
         </div>
         <div>プログレスバー</div>
       </div>
