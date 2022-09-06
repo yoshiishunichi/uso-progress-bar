4445420fc02aea0f52e8869d78474b03a1d34600
吉井駿一 <syun_syun01@icloud.com>
2022-09-06 23:46:09 +0900

    :sparkles: 閉じるボタンの背景のみを追加

--- src/components/ProgressView.tsx
+++ src/components/ProgressView.tsx
@@ -12,8 +12,9 @@ const ProgressView: FC<ProgressViewProps> = ({ fetchingState }) => {
   return fetching || error ? (
     <div className="absolute inset-0 flex items-center justify-center bg-frosted">
       <div className="h-28 w-96 rounded-sm border border-black bg-slate-100">
-        <div className="flex h-6 items-center justify-center border-b border-black bg-gradient-to-b from-header-thin to-header-dark">
+        <div className="relative flex h-6 items-center justify-center border-b border-black bg-gradient-to-b from-header-thin to-header-dark">
           <p className="text-center text-xs">Progress Bar</p>
+          <button className="absolute left-1 h-3 w-3 rounded-full bg-close"></button>
         </div>
         <div>プログレスバー</div>
       </div>
