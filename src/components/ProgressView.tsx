de1b0db7482f8aaceca4f074c36e41fde5f6a865
吉井駿一 <syun_syun01@icloud.com>
2022-09-07 00:31:53 +0900

    :sparkles: バーを伸ばす処理以外完成

--- src/components/ProgressView.tsx
+++ src/components/ProgressView.tsx
@@ -12,8 +12,8 @@ const ProgressView: FC<ProgressViewProps> = ({ fetchingState }) => {
 
   return fetching || error ? (
     <div className="absolute inset-0 flex items-center justify-center bg-frosted">
-      <div className="h-28 w-96 rounded-sm border border-black bg-slate-100">
-        <div className="relative flex h-8 items-center justify-center border-b border-black bg-gradient-to-b from-header-thin to-header-dark">
+      <div className="h-28 w-5/6 max-w-xl rounded-sm border border-black bg-white">
+        <div className="relative flex h-6 items-center justify-center border-b border-black bg-gradient-to-b from-header-thin to-header-dark">
           <p className="text-center text-xs">Progress Bar</p>
           <button className="absolute left-2 h-4 w-4 rounded-full bg-close">
             <div className="flex h-4 w-4 items-center justify-center">
@@ -21,7 +21,15 @@ const ProgressView: FC<ProgressViewProps> = ({ fetchingState }) => {
             </div>
           </button>
         </div>
-        <div>プログレスバー</div>
+        <div className="mx-auto flex w-11/12 flex-col justify-center gap-2">
+          <div>
+            <div className="mx-auto mt-4 h-5 rounded-full border border-black bg-gradient-to-b from-bar-dark to-bar-thin shadow-xl"></div>
+          </div>
+          <div className="flex items-center gap-1">
+            <div className="h-4 w-4 animate-spin rounded-full border-2 border-slate-400 border-t-transparent"></div>
+            <p className="text-sm text-slate-900">すごく長〜い処理を実行中…</p>
+          </div>
+        </div>
       </div>
     </div>
   ) : (
